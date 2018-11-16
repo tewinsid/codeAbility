005f65726b3b661729a17592b75c3e3c3cd7efbf
tewinsid <vipwanggang@outlook.com>
2018-11-16 09:34:54 +0800

    '20181116'

--- src/leetcode/easy/TwoSum.java
+++ src/leetcode/easy/TwoSum.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 /**
  * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
  * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+ * tips:
+ *  推荐返回值临时创建
+ *  尽量把操作靠近它生效的位置
+ *  当在循环中return的情况下可以在结尾抛出异常替代return
  */
 public class TwoSum {
 
@@ -18,17 +22,13 @@ public class TwoSum {
     }
 
     public int[] twoSUm(int[] nums, int target) {
-        int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            int temp = target - nums[i];
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == temp) {
-                    result[0] = i;
-                    result[1] = j;
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
                 }
             }
         }
-        return result;
+        throw new IllegalArgumentException("No two sum solution");
     }
-
 }
